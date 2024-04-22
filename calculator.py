fba6f226f6a4ff652503f5341d42f039fc43f634
Thiago Pancieri <thiagopancieri@gmail.com>
2024-04-22 01:39:05 -0300

    made the calculator program more modular

--- calculator.py
+++ calculator.py
@@ -1,24 +1,35 @@
 import typing
 
+def add(number1, number2) -> typing.Union[int, float]:
+    return number1 + number2
+
+def subtract(number1, number2) -> typing.Union[int, float]:
+    return number1 - number2
+
+def multiply(number1, number2) -> typing.Union[int, float]:
+    return number1 * number2
+
+def divide(number1, number2) -> typing.Union[int, float]:
+    if number2 == 0:
+        raise ZeroDivisionError("Cannot divide by zero!")
+    return number1 / number2
+
 def calculate(number1, operator, number2) -> typing.Union[int, float]:
-    if operator == "+":
-        return number1 + number2     
-    elif operator == "-":
-        return number1 - number2     
-    elif operator == "*":
-        return number1 * number2     
-    elif operator == "/":
-        if number2 == 0:
-            raise ZeroDivisionError("Cannot divide by zero!")
-        return number1 / number2     
-    else: 
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide
+    }
+    if operator not in operations:
         raise ValueError("Invalid operator!")
+    return operations[operator](number1, number2)
 
 def main():
     while True:
         try:
             number1 = int(input("What's the first number?"))
-            operator = input("What's the operator?")
+            operator = input("What's the operator? (+, -, *, /)")
             number2 = int(input("What's the second number?"))
 
             if operator not in "+-*/":
@@ -38,7 +49,8 @@ def main():
                 print("Invalid choice")
         except ValueError as e:
             print(f"Error:  {e}")
-
+        except ZeroDivisionError as e:
+            print(f"Error:  {e}")
 
 if __name__ == "__main__":
     main()
